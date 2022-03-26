55fa39bc3caadeaea77348599262a6f0e47509ab
Ian Findlay <code@findlayian.com>
2022-03-26 23:31:42 +0000

    Use env variable for hosted credentials

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const { handleCustomErrors } = require("./controllers/errors.controller");
-
+const cors = require("cors");
 const express = require("express");
 
 const apiRouter = require("./routes/api-router");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,20 @@
 const fs = require("firebase-admin");
 
-const serviceAccount = require("../serviceAccountKey.json");
+const { NODE_ENV = test } = process.env;
 
-fs.initializeApp({
-  credential: fs.credential.cert(serviceAccount),
-});
+if (NODE_ENV === "production") {
+  const firebaseCredentials = JSON.parse(
+    Buffer.from(process.env.FIREBASE_CREDS, "base64").toString("ascii")
+  );
+  fs.initializeApp({
+    credential: fs.credential.cert(firebaseCredentials),
+  });
+} else {
+  const serviceAccount = require("../serviceAccountKey.json");
+  fs.initializeApp({
+    credential: fs.credential.cert(serviceAccount),
+  });
+}
 
 const db = fs.firestore();
 
