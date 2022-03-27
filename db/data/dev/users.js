300dd8e33706444e0f9f9d271357342e28f50ea4
Ian Findlay <code@findlayian.com>
2022-03-27 19:28:23 +0100

    Make some dev data

--- db/data/dev/users.js
+++ db/data/dev/users.js
@@ -1,6 +1,6 @@
 module.exports = [
   {
-    username: "username3",
+    username: "Boorish Johnson",
     postcode: "SW1A2AA",
     img: "https://firebasestorage.googleapis.com/v0/b/petloans-b9778.appspot.com/o/images%2FWtJL2gZ9vGRYuFfQ2k79hH4Fc7F2%2F1648303599784.jpg?alt=media&token=779a48a4-44a6-4a03-b17a-b88f9a9edddf ",
   },
